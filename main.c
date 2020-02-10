2482ebce1050ed428892019823f1b8e549934105
ValentinZeller <v.zeller@ludus-academie.com>
2020-02-10 16:42:39 +0100

    Question 7

--- main.c
+++ main.c
@@ -19,5 +19,21 @@ int main(int argc, char *argv[]) //Modification du main pour inclure le main de 
         return 1;
     }
 
+    pWindow = SDL_CreateWindow("Une fenetre SDL",SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_RESIZABLE);
+        // Création de la fenêtre avec SDL_CreateWindow (titre de la fenêtre, position horizontale, position verticale, largeur, hauteur et les flags
+        //SDL_WINDOWS_CENTERED permet de centrer la fenêtre
+        //SDL_WINDOW_RESIZABLE permet de changer la taille de la fenêtre pendant l'execution
+
+    if (pWindow == NULL)  //Si la fenêtre est vide, on stop le programme
+    {
+        printf("Erreur lors de la creation d'une fenetre : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Delay(3000);  //Pause de 3 secondes pour voir la fenêtre
+
+    SDL_DestroyWindow(pWindow); //Suppression de la fenêtre
+    SDL_Quit();  //Fin de la SDL
     return 0;
 }
